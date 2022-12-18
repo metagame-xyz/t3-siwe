634209e6f11d1e077e235b6d3caeaa0768764635
Brenner Spear <blspear@gmail.com>
2022-12-18 17:01:01 -0600

    update prettier, tsconfig, add trivago

--- src/server/trpc/router/example.ts
+++ src/server/trpc/router/example.ts
@@ -1,16 +1,16 @@
-import { z } from "zod";
+import { z } from 'zod'
 
-import { router, publicProcedure } from "../trpc";
+import { publicProcedure, router } from '../trpc'
 
 export const exampleRouter = router({
-  hello: publicProcedure
-    .input(z.object({ text: z.string().nullish() }).nullish())
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input?.text ?? "world"}`,
-      };
+    hello: publicProcedure
+        .input(z.object({ text: z.string().nullish() }).nullish())
+        .query(({ input }) => {
+            return {
+                greeting: `Hello ${input?.text ?? 'world'}`,
+            }
+        }),
+    getAll: publicProcedure.query(({ ctx }) => {
+        return ctx.prisma.example.findMany()
     }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
-});
+})
